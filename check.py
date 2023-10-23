850641c7f1ef09c1cede13a698fd8942e956616c
Emmanuel C <eechukwu@gmail.com>
2023-10-23 15:32:13 +0100

    Update check.py

--- check.py
+++ check.py
@@ -1,17 +1,12 @@
 import subprocess
 import json
 from urllib.parse import urlparse
-
-# ANSI escape sequences for colored output
-class ANSIColors:
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    ENDC = '\033[0m'
+from termcolor import colored
 
 # Define your clusters here
 CLUSTERS = [
-    {"name": "non-prod-buying", "project": "xxxxxx", "context": ""},
-    # Add other clusters...
+    {"name": "non-prod-warehouse", "project": "corp-test-mgmt-anthos-3578", "context": "connectgateway_corp-test-mgmt-anthos-3578_global_non-prod-warehouse"},
+    # Add other clusters here
 ]
 
 def switch_context(cluster_info):
@@ -22,94 +17,76 @@ def switch_context(cluster_info):
     out, err = proc.communicate()
 
     if proc.returncode != 0:
-        print(f"Error switching to context {context}: {err.decode()}")
+        print(f"Error switching to context {context}: {err.decode()}")  # Decode error message
         return False
 
-    print(f"Switched to context {context}")
+    print(f"Switched to context {context}")  # Debugging print
     return True
 
-def get_ingresses():
+def fetch_ingresses():
     print("Fetching ingresses...")  # Debugging print
-    cmd = ['kubectl', 'get', 'ingresses', '-o', 'json']
+    cmd = ['kubectl', 'get', 'ingresses', '--all-namespaces', '-o', 'json']
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = proc.communicate()
 
     if proc.returncode != 0:
-        print(f"Error fetching ingresses: {err.decode()}")
+        print(f"Error fetching ingresses: {err.decode()}")  # Decode error message
         return None
 
-    ingresses = json.loads(out.decode())
+    ingresses = json.loads(out)
     print(f"Fetched ingresses: {ingresses}")  # Debugging print
-    return ingresses
 
-def ingress_exists(host, ingresses):
-    print(f"Checking if ingress exists for host: {host}")  # Debugging print
+    # Print all the ingresses in the cluster, each on a new line
     for ingress in ingresses['items']:
-        for rule in ingress.get('spec', {}).get('rules', []):
-            if host == rule.get('host'):
-                print(f"Found matching ingress for host: {host}")  # Debugging print
-                return True
-    print(f"No ingress found for host: {host}")  # Debugging print
-    return False
+        print(json.dumps(ingress, indent=4))  # Print each ingress with nice formatting
+
+    return ingresses
 
 def check_endpoint(url, ingresses):
     print(f"Checking URL: {url}")  # Debugging print
     host = urlparse(url).netloc
-    cmd = ['curl', '-ks', '-o', '/dev/null', '-w', '%{http_code}', url]
+    ingress_found = any(ingress for ingress in ingresses['items'] if any(rule['host'] == host for rule in ingress['spec']['rules']))
 
+    cmd = ['curl', '-ks', '-o', '/dev/null', '-w', '%{http_code}', url]
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = proc.communicate()
 
     if proc.returncode != 0:
-        print(f"Error checking {url}: {err.decode()}")
+        print(f"Error checking {url}: {err.decode()}")  # Decode error message
         return None
 
     status_code = out.decode().strip()
     print(f"Status code for {url}: {status_code}")  # Debugging print
 
-    # Handle response based on status code
-    if status_code == "200":
-        return f"{ANSIColors.GREEN}{url}: '200' - SSL Working Fine{ANSIColors.ENDC}"
-    elif status_code == "404":
-        if ingress_exists(host, ingresses):
-            return f"{ANSIColors.RED}{url}: '404' - Error 404 returned, please investigate{ANSIColors.ENDC}"
-        else:
-            return f"{url}: '404' - Appname is an internal app, no ingress found"
+    if status_code == '200':
+        return colored(f"{url}: '{status_code}' - SSL Working Fine", "green")
+    elif status_code == '404' and not ingress_found:
+        return colored(f"{url}: '404' - No ingress found", "yellow")
+    elif status_code == '404':
+        return colored(f"{url}: '404' - Error 404 returned, please investigate", "red")
     else:
-        return f"{ANSIColors.RED}{url}: '{status_code}' - Unexpected status code returned{ANSIColors.ENDC}"
+        return colored(f"Unexpected output for {url}: '{status_code}'", "red")
 
 def main():
     print("Script started...")  # Debugging print
-    for cluster_info in CLUSTERS:
-        if switch_context(cluster_info):
-            ingresses = get_ingresses()
-            if ingresses:
-                urls = []  # populate this with the URLs you want to check
-                for ingress in ingresses['items']:
-                    for rule in ingress.get('spec', {}).get('rules', []):
-                        host = rule.get('host')
-                        if host:  # assuming http for simplicity, adjust as needed
-                            urls.append(f"http://{host}/health")
 
-                for url in urls:
-                    result = check_endpoint(url, ingresses)
-                    if result:
-                        print(result)
-            else:
-                print(f"{ANSIColors.RED}No ingresses found in the cluster: {cluster_info['name']}{ANSIColors.ENDC}")
-        else:
-            print(f"Skipping checks for cluster: {cluster_info['name']}")
+    for cluster in CLUSTERS:
+        if not switch_context(cluster):
+            continue  # if context switch fails, continue to the next cluster
+
+        ingresses = fetch_ingresses()
+        if not ingresses:
+            continue  # if fetching ingresses fails, continue to the next cluster
+
+        for ingress in ingresses['items']:
+            for rule in ingress['spec']['rules']:
+                host = rule['host']
+                url = f"https://{host}/health"
+                result = check_endpoint(url, ingresses)
+                if result:
+                    print(result)
 
     print("Script execution finished.")  # Debugging print
 
 if __name__ == "__main__":
     main()
-Script started...
-Switching context to: {'name': 'non-prod-warehouse', 'project': 'corp-test-mgmt-anthos-3578', 'context': 'connectgateway_corp-test-mgmt-anthos-3578_global_non-prod-warehouse'}
-Switched to context connectgateway_corp-test-mgmt-anthos-3578_global_non-prod-warehouse
-Fetching ingresses...
-Fetched ingresses: {'apiVersion': 'v1', 'items': [{'apiVersion': 'networking.k8s.io/v1', 'kind': 'Ingress', 'metadata': {'annotations': {'kubectl.kubernetes.io/last-applied-configuration': '{"apiVersion":"networking.k8s.io/v1","kind":"Ingress","metadata":{"annotations":{},"name":"apache-ingress","namespace":"default"},"spec":{"rules":[{"host":"apache-test-f5.anthos.sportski.com","http":{"paths":[{"backend":{"service":{"name":"apache","port":{"number":80}}},"path":"/","pathType":"Prefix"}]}}]}}\n'}, 'creationTimestamp': '2023-07-11T09:20:34Z', 'generation': 1, 'name': 'apache-ingress', 'namespace': 'default', 'resourceVersion': '136106790', 'uid': '3b2b5e3b-8f88-4fdd-9bd9-4a07fc0e4fb8'}, 'spec': {'rules': [{'host': 'apache-test-f5.anthos.sportski.com', 'http': {'paths': [{'backend': {'service': {'name': 'apache', 'port': {'number': 80}}}, 'path': '/', 'pathType': 'Prefix'}]}}]}, 'status': {'loadBalancer': {'ingress': [{'ip': '172.16.179.25'}]}}}], 'kind': 'List', 'metadata': {'resourceVersion': ''}}
-Checking URL: http://apache-test-f5.anthos.sportski.com/health
-Error checking http://apache-test-f5.anthos.sportski.com/health: 
-Script execution finished.
-
