1e862ada29d5f335ff53514673dd76360ffc1582
Emmanuel C <eechukwu@gmail.com>
2023-10-23 16:03:19 +0100

    Update listend.py

--- listend.py
+++ listend.py
@@ -3,7 +3,7 @@ import json
 
 CLUSTERS = [
     {"name": "non-prod-buying", "project": "xxxxxx", "context": ""},
-    # Add other clusters here with their respective context
+    # Add other clusters here...
 ]
 
 def switch_context(cluster_info):
@@ -19,23 +19,11 @@ def switch_context(cluster_info):
     print(f"Switched to context {context}")
     return True
 
-def get_deployments():
+def get_all_ingresses():
     try:
-        deployments_raw = subprocess.check_output(["kubectl", "get", "deployments", "--all-namespaces", "-o=json"], text=True)
-        deployments_json = json.loads(deployments_raw)
-        return deployments_json['items']
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while retrieving deployments: {str(e)}")
-        return []
-    except json.JSONDecodeError as e:
-        print(f"An error occurred while parsing JSON: {str(e)}")
-        return []
-
-def get_ingresses():
-    try:
-        ingress_raw = subprocess.check_output(["kubectl", "get", "ingresses", "--all-namespaces", "-o=json"], text=True)
-        ingress_json = json.loads(ingress_raw)
-        return ingress_json['items']
+        ingresses_raw = subprocess.check_output(["kubectl", "get", "ingresses", "--all-namespaces", "-o=json"], text=True)
+        ingresses_json = json.loads(ingresses_raw)
+        return ingresses_json['items']
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while retrieving ingresses: {str(e)}")
         return []
@@ -43,33 +31,29 @@ def get_ingresses():
         print(f"An error occurred while parsing JSON: {str(e)}")
         return []
 
-def main():
-    if not CLUSTERS:
-        print("No clusters defined.")
-        return
+def curl_endpoint(endpoint):
+    try:
+        response = subprocess.check_output(["curl", "-Is", endpoint], text=True)
+        print(f"Response from {endpoint}:")
+        print(response)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while curling {endpoint}: {str(e)}")
 
+def main():
     for cluster in CLUSTERS:
         if not cluster['context']:
             print(f"Context for cluster {cluster['name']} is not defined. Skipping...")
             continue
         if switch_context(cluster):
-            print(f"Fetching applications and ingresses for cluster: {cluster['name']}")
-            ingresses = get_ingresses()
-            apps_with_ingress = set()
+            print(f"Fetching ingresses for cluster: {cluster['name']}")
+
+            ingresses = get_all_ingresses()
             for ingress in ingresses:
-                app_name = ingress['metadata']['name']
-                apps_with_ingress.add(app_name)
-                for rule in ingress['spec']['rules']:
-                    host = rule['host']
-                    print(f"App with Ingress: {app_name}, Endpoint: {host}")
-
-            deployments = get_deployments()
-            all_apps = set(deployment['metadata']['name'] for deployment in deployments)
-            apps_without_ingress = all_apps - apps_with_ingress
-
-            print("\nApps without Ingress:")
-            for app in apps_without_ingress:
-                print(app)
+                for rule in ingress.get('spec', {}).get('rules', []):
+                    host = rule.get('host', '')
+                    if host:
+                        print(f"Curling: {host}")
+                        curl_endpoint(f"http://{host}")  # Assumes HTTP. Change to 'https://' if endpoints are HTTPS.
 
 if __name__ == '__main__':
     main()
