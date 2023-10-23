e05788e46518d0a8b76cf3be4d67846339ff61d4
Emmanuel C <eechukwu@gmail.com>
2023-10-23 17:19:49 +0100

    Update listend.py

--- listend.py
+++ listend.py
@@ -19,58 +19,49 @@ def switch_context(cluster_info):
     print(f"Switched to context {context}")
     return True
 
-def get_ingress_endpoints():
+def get_ingresses():
     cmd = ['kubectl', 'get', 'ing', '-o', 'json']
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = proc.communicate()
 
     if proc.returncode != 0:
         print(f"Error getting ingresses: {err.decode('utf-8')}")
-        return None
+        return []
 
     ingresses = json.loads(out.decode('utf-8'))
-    endpoints = []
-
-    for item in ingresses['items']:
-        # Assuming the first rule hosts the service endpoint
-        try:
-            rules = item['spec']['rules']
-            for rule in rules:
-                host = rule['host']
-                if host.startswith('http://'):
-                    continue  # Ignore non-HTTPS URLs
-                https_url = f"https://{host}/health"
-                endpoints.append(https_url)
-        except KeyError:
-            continue  # Ingress might not have a 'rules' field
-
-    return endpoints
+    return ingresses['items']
 
 def check_endpoint_health(endpoint):
-    cmd = ['curl', '-o', '/dev/null', '-s', '-w', '%{http_code}', '--insecure', endpoint]  # '--insecure' for SSL issues
+    cmd = ['curl', '-o', '/dev/null', '-s', '-w', '%{http_code}', '--insecure', endpoint]
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     out, err = proc.communicate()
 
     if proc.returncode != 0:
-        print(f"Error curling {endpoint}: {err.strip()}")  # Print the error message from curl
-        return "Error"  # You can return a more descriptive error message if needed
+        return f"Error: {err.strip()}"
 
     return out.strip()
 
 def main():
     for cluster in CLUSTERS:
-        success = switch_context(cluster)
-        if not success:
-            continue
+        if not switch_context(cluster):
+            continue  # if switching context fails, continue to next cluster
 
-        print(f"Checking endpoints for cluster {cluster['name']}...")
-        endpoints = get_ingress_endpoints()
-        if endpoints is None:
-            continue
+        ingresses = get_ingresses()
+        for ing in ingresses:
+            annotations = ing['metadata'].get('annotations', {})
+            if 'nginx.ingress.kubernetes.io/rewrite-target' in annotations:
+                continue  # skip ingresses that have a rewrite-target
 
-        for endpoint in endpoints:
-            status = check_endpoint_health(endpoint)
-            print(f'"{endpoint}": "{status}"')
+            for rule in ing['spec']['rules']:
+                host = rule['host']
+                for path in rule.get('http', {}).get('paths', []):
+                    path_val = path['path']
+                    path_val = path_val if path_val.endswith('/') else path_val + '/'
+                    url = f"https://{host}{path_val}health"  # construct the URL
+
+                    if url.startswith('https://'):
+                        status = check_endpoint_health(url)
+                        print(f'"{url}": "{status}"')
 
 if __name__ == "__main__":
     main()
