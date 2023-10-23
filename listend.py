472c4cb5413cfaf202b42217e83b80f2330ca58f
Emmanuel C <eechukwu@gmail.com>
2023-10-23 16:18:52 +0100

    Update listend.py

--- listend.py
+++ listend.py
@@ -2,59 +2,56 @@ import subprocess
 import json
 
 CLUSTERS = [
-    {"name": "non-prod-buying", "project": "xxxxxx", "context": ""},
-    # Add other clusters here...
+    {"name": "non-prod-buying", "project": "xxxxxx", "context": "your-context-name"},
+    # Add other clusters here with their respective context names...
 ]
 
 def switch_context(cluster_info):
     context = cluster_info['context']
     cmd = ['kubectl', 'config', 'use-context', context]
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = proc.communicate()
-
-    if proc.returncode != 0:
-        print(f"Error switching to context {context}: {err.decode('utf-8')}")
+    try:
+        subprocess.run(cmd, check=True, capture_output=True, text=True)
+        print(f"Switched to context {context}")
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"Error switching to context {context}: {e.stderr}")
         return False
 
-    print(f"Switched to context {context}")
-    return True
-
-def get_services():
-    services = []
-    cmd = ['kubectl', 'get', 'svc', '--all-namespaces', '-o', 'json']
-    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = proc.communicate()
-
-    if proc.returncode == 0:
-        services_json = json.loads(out.decode('utf-8'))
-        for service in services_json['items']:
-            try:
-                app_name = service['metadata']['labels']['app']
-                ingress = service['spec']['ports'][0]['nodePort']
-                services.append((app_name, ingress))
-            except (KeyError, IndexError):
-                # Ignoring services that do not match the expected structure
-                pass
-    else:
-        print(f"Error retrieving services: {err.decode('utf-8')}")
-
-    return services
-
-def curl_endpoint(endpoint):
+def get_endpoints():
+    cmd = ['kubectl', 'get', 'ing', '--all-namespaces', '-o', 'json']
     try:
-        response = subprocess.check_output(["curl", "-o", "/dev/null", "-Is", "-w", "%{http_code}", endpoint], text=True)
-        print(f'"{endpoint}" : "{response.strip()}"')
+        result = subprocess.run(cmd, check=True, capture_output=True, text=True)
+        ingress_data = json.loads(result.stdout)
+        endpoints = []
+
+        for item in ingress_data['items']:
+            try:
+                host = item['spec']['rules'][0]['host']
+                https_scheme = "https://" if item['spec'].get('tls') else "http://"  # Check if TLS is enabled
+                endpoints.append(f"{https_scheme}{host}/health")
+            except (KeyError, IndexError):
+                continue  # Skip ingresses without the necessary structure
+
+        return endpoints
     except subprocess.CalledProcessError as e:
-        print(f'An error occurred while curling {endpoint}: {str(e)}')
+        print(f"Error retrieving ingresses: {e.stderr}")
+        return []
+
+def check_endpoint_health(endpoint):
+    cmd = ['curl', '-o', '/dev/null', '-Is', '-w', '%{http_code}', endpoint]
+    try:
+        result = subprocess.run(cmd, check=True, capture_output=True, text=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        return f"Error: {e.stderr}"
 
 def main():
     for cluster in CLUSTERS:
         if switch_context(cluster):
-            services = get_services()
-            for service in services:
-                app_name, ingress = service
-                url = f"https://{app_name}.your-cluster-domain.com:{ingress}/health"
-                curl_endpoint(url)
+            endpoints = get_endpoints()
+            for endpoint in endpoints:
+                status_code = check_endpoint_health(endpoint)
+                print(f'"{endpoint}": "{status_code}"')
 
 if __name__ == "__main__":
     main()
