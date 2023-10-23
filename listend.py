d48f1c45897975bd989a195ecfa0fd81213f555a
Emmanuel C <eechukwu@gmail.com>
2023-10-23 15:56:29 +0100

    Update listend.py

--- listend.py
+++ listend.py
@@ -19,6 +19,18 @@ def switch_context(cluster_info):
     print(f"Switched to context {context}")
     return True
 
+def get_deployments():
+    try:
+        deployments_raw = subprocess.check_output(["kubectl", "get", "deployments", "--all-namespaces", "-o=json"], text=True)
+        deployments_json = json.loads(deployments_raw)
+        return deployments_json['items']
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while retrieving deployments: {str(e)}")
+        return []
+    except json.JSONDecodeError as e:
+        print(f"An error occurred while parsing JSON: {str(e)}")
+        return []
+
 def get_ingresses():
     try:
         ingress_raw = subprocess.check_output(["kubectl", "get", "ingresses", "--all-namespaces", "-o=json"], text=True)
@@ -41,13 +53,23 @@ def main():
             print(f"Context for cluster {cluster['name']} is not defined. Skipping...")
             continue
         if switch_context(cluster):
-            print(f"Fetching ingresses for cluster: {cluster['name']}")
+            print(f"Fetching applications and ingresses for cluster: {cluster['name']}")
             ingresses = get_ingresses()
+            apps_with_ingress = set()
             for ingress in ingresses:
                 app_name = ingress['metadata']['name']
+                apps_with_ingress.add(app_name)
                 for rule in ingress['spec']['rules']:
                     host = rule['host']
-                    print(f"App: {app_name}, Endpoint: {host}")
+                    print(f"App with Ingress: {app_name}, Endpoint: {host}")
+
+            deployments = get_deployments()
+            all_apps = set(deployment['metadata']['name'] for deployment in deployments)
+            apps_without_ingress = all_apps - apps_with_ingress
+
+            print("\nApps without Ingress:")
+            for app in apps_without_ingress:
+                print(app)
 
 if __name__ == '__main__':
     main()
